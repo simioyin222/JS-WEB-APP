03fb68b435e9faa0ae2e6bff40df0454493098b8
Simi O <coolkid1984@ymail.com>
2023-08-11 16:16:11 -0500

    add variables to javascript main query selction tags

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
 document.getElementById("selection-form").addEventListener("submit", function(event) {
   event.preventDefault();
-  const devType = document.querySelector()
+  const devType = document.querySelector("#devType").value;
+  const goalType = document.querySelector("#machinelearning").value;
+  const machineLearning = document.querySelector("#learningStyle").value;
+  const learningStyle = document.querySelector("#learningstyle").value;
+  const performance = document.querySelector("#performance").value;
+
+  const result
 }
