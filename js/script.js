e6c517f647c773a582c24cef7f1d9513e07cd155
Simi O <coolkid1984@ymail.com>
2023-08-11 18:07:34 -0500

    add any missing corrections and format the code correctly so it more readable

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 document.getElementById("selection-form").addEventListener("submit", function(event) {
   event.preventDefault();
+
   const devType = document.querySelector("#devType").value;
   const goalType = document.querySelector("#goalType").value;
   const machineLearning = document.querySelector("#machinelearning").value;
@@ -14,19 +15,19 @@ document.getElementById("selection-form").addEventListener("submit", function(ev
         resultSection.textContent = "We suggest Javascript!";
       }
     }
-  }
-} else if (devType === "app") {
-  if (goalType === "yes") {
-    if (performance === "no") {
-      resultSection.textContent = "We suggest C#!";
+  } else if (devType === "app") {
+    if (goalType === "yes") {
+      if (performance === "no") {
+        resultSection.textContent = "We suggest C#!";
+      }
     }
+  } else if (machineLearning === "yes") {
+    if (learningStyle === "yes") {
+      resultSection.textContent = "We suggest Python!";
+    }
+  } else {
+    resultSection.textContent = "Maybe consider learning none of them and just giving up!";
   }
-} else if (machineLearning === "yes") {
-  if (learningStyle === "yes") {
-    resultSection.textContent = "We suggest Python!";
-  }
-} else {
-  resultSection.textContent = "Maybe consider learning none of them and just giving up!";
-}
 
-resultSection.classlist.remove("hidden");
+  resultSection.classList.remove("hidden");
+});
