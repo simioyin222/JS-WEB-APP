ec2ba64d696882ddade23de3354c8c4926f7b151
Simi O <coolkid1984@ymail.com>
2023-08-11 19:00:44 -0500

    add all final pages, still not loading anwser

--- js/script.js
+++ js/script.js
@@ -3,30 +3,31 @@ document.getElementById("selection-form").addEventListener("submit", function(ev
 
   const devType = document.querySelector("#devType").value;
   const goalType = document.querySelector("#goalType").value;
-  const machineLearning = document.querySelector("#machinelearning").value;
-  const learningStyle = document.querySelector("#learningstyle").value;
+  const machineLearning = document.querySelector("#machineLearning").value;
+  const learningStyle = document.querySelector("#learningStyle").value;
   const performance = document.querySelector("#performance").value;
 
   const resultSection = document.getElementById("result");
+  resultSection.textContent = "";
 
   if (devType === "web") {
-    if (goalType === "no") {
-      if (learningStyle === "yes") {
-        resultSection.textContent = "We suggest Javascript!";
-      }
-    }
+    if (machineLearning === "yes") {
+      resultSection.textContent = "We suggest JavaScript with TensorFlow.js!";
+    } else if (goalType === "no") {
+      resultSection.textContent = "We suggest PHP!";
+    } 
   } else if (devType === "app") {
-    if (goalType === "yes") {
-      if (performance === "no") {
-        resultSection.textContent = "We suggest C#!";
-      }
+    if (performance === "no") {
+      resultSection.textContent = "We suggest Java!";
+    } else if (performance === "yes") {
+      resultSection.textContent = "We suggest Swift!";
     }
   } else if (machineLearning === "yes") {
-    if (learningStyle === "yes") {
+    if (learningStyle === "read") {
       resultSection.textContent = "We suggest Python!";
-    }
+    } 
   } else {
-    resultSection.textContent = "Maybe consider learning none of them and just giving up!";
+    resultSection.textContent = "Explore based on your personal interests!";
   }
 
   resultSection.classList.remove("hidden");
