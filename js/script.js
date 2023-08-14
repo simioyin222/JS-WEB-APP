75c12117237f320e91d822fa6cae3fbfcacd264a
Simi O <coolkid1984@ymail.com>
2023-08-13 20:54:12 -0500

    add function to javascript page after declaring it with varible const to start now but keeping let at secondary varible

--- js/script.js
+++ js/script.js
@@ -1,12 +1,31 @@
-document.getElementById("selection-form");
-document.addEventListener("submit", function (event) {
+const formElement = document.getElementById("selection-form");
+
+formElement.addEventListener("submit", function (event) {
   event.preventDefault();
 
-  let devType = document.querySelector("devType").value;
-  let goalType = document.querySelector("goalType").value;
-  let machineLearning = document.querySelector("machineLearning").value;
-  let learningStyle = document.querySelector("learningStyle").value;
-  let performance = document.querySelector("performance").value;
+
+  let devType = document.querySelector("#devType").value;
+  let goalType = document.querySelector("#goalType").value;
+  let machineLearning = document.querySelector("#machineLearning").value;
+  let learningStyle = document.querySelector("#learningStyle").value;
+  let performance = document.querySelector("#performance").value;
 
   let resultSection = document.getElementById("result");
-  resultSection.textContent = "";
+  let result = "";
+
+
+  if (devType === "web") {
+    if (goalType === "yes") {
+      if (machineLearning === "yes") {
+        if (learningStyle === "read") {
+          if (performance === "yes") {
+            result =
+              "Consider trying Python & I'm not talking about the snake hahah!";
+          }
+        }
+      }
+    }
+  }
+
+  resultSection.textContent = result;
+});
