0c2a6e25909739d5078bfbe19cfbaf6ab44e1309
Simi O <coolkid1984@ymail.com>
2023-08-13 21:06:53 -0500

    "add new javascript page"

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ const formElement = document.getElementById("selection-form");
 formElement.addEventListener("submit", function (event) {
   event.preventDefault();
 
-
   let devType = document.querySelector("#devType").value;
   let goalType = document.querySelector("#goalType").value;
   let machineLearning = document.querySelector("#machineLearning").value;
@@ -13,14 +12,36 @@ formElement.addEventListener("submit", function (event) {
   let resultSection = document.getElementById("result");
   let result = "";
 
-
   if (devType === "web") {
     if (goalType === "yes") {
       if (machineLearning === "yes") {
         if (learningStyle === "read") {
           if (performance === "yes") {
-            result =
-              "Consider trying Python & I'm not talking about the snake hahah!";
+            result = "Consider trying Python!";
+          }
+        }
+      }
+    }
+  }
+
+  if (devType === "web") {
+    if (goalType === "yes") {
+      if (machineLearning === "no") {
+        if (learningStyle === "practice") {
+          if (performance === "yes") {
+            result = "Consider trying JavaScript!";
+          }
+        }
+      }
+    }
+  }
+
+  if (devType === "app") {
+    if (goalType === "yes") {
+      if (machineLearning === "yes") {
+        if (learningStyle === "read") {
+          if (performance === "no") {
+            result = "Consider trying C#!";
           }
         }
       }
