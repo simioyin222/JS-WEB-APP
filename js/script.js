39548c845b6d24cad58c32041546d360228901f0
Simi O <coolkid1984@ymail.com>
2023-08-13 20:37:05 -0500

    add new varibles to important factors in javascript page = const has now been changed to let so that it has freedom to change

--- js/script.js
+++ js/script.js
@@ -1,34 +1,12 @@
-document.getElementById("selection-form").addEventListener("submit", function(event) {
+document.getElementById("selection-form");
+document.addEventListener("submit", function (event) {
   event.preventDefault();
 
-  const devType = document.querySelector("#devType").value;
-  const goalType = document.querySelector("#goalType").value;
-  const machineLearning = document.querySelector("#machineLearning").value;
-  const learningStyle = document.querySelector("#learningStyle").value;
-  const performance = document.querySelector("#performance").value;
+  let devType = document.querySelector("devType").value;
+  let goalType = document.querySelector("goalType").value;
+  let machineLearning = document.querySelector("machineLearning").value;
+  let learningStyle = document.querySelector("learningStyle").value;
+  let performance = document.querySelector("performance").value;
 
-  const resultSection = document.getElementById("result");
+  let resultSection = document.getElementById("result");
   resultSection.textContent = "";
-
-  if (devType === "web") {
-    if (machineLearning === "yes") {
-      resultSection.textContent = "We suggest JavaScript with TensorFlow.js!";
-    } else if (goalType === "no") {
-      resultSection.textContent = "We suggest PHP!";
-    } 
-  } else if (devType === "app") {
-    if (performance === "no") {
-      resultSection.textContent = "We suggest Java!";
-    } else if (performance === "yes") {
-      resultSection.textContent = "We suggest Swift!";
-    }
-  } else if (machineLearning === "yes") {
-    if (learningStyle === "read") {
-      resultSection.textContent = "We suggest Python!";
-    } 
-  } else {
-    resultSection.textContent = "Explore based on your personal interests!";
-  }
-
-  resultSection.classList.remove("hidden");
-});
